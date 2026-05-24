e02b69bf57f6b22edadbfb23061da0c36fe8d677
norikenpi <norikenpi@gmail.com>
2026-05-24 23:46:49 +0900

    feat: register K1 locomotion deploy tasks

--- tasks/locomotion/__init__.py
+++ tasks/locomotion/__init__.py
@@ -16,5 +16,50 @@ class T1WalkControllerCfg1(T1WalkControllerCfg):
         self.policy.checkpoint_path = "models/t1_walk.pt"
 
 
+@configclass
+class K1WalkControllerCfg1(K1WalkControllerCfg):
+    """Velocity-commanded walk for K1 robot."""
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.policy.checkpoint_path = "models/k1_locomotion.pt"
+
+
+@configclass
+class K1WalkCurrentTrainControllerCfg(K1WalkControllerCfg):
+    """K1 walk using the actuator profile from the current Isaac Lab training."""
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.policy.checkpoint_path = "models/k1_locomotion.pt"
+        self.robot.joint_stiffness = [
+            3.9478417602100686, 3.9478417602100686,
+            3.9478417602100686, 3.9478417602100686, 3.9478417602100686, 3.9478417602100686,
+            3.9478417602100686, 3.9478417602100686, 3.9478417602100686, 3.9478417602100686,
+            30.200989465607023, 21.447961045805584, 17.846013389258083,
+            60.401978931214046, 35.692026778516166, 35.692026778516166,
+            30.200989465607023, 21.447961045805584, 17.846013389258083,
+            60.401978931214046, 35.692026778516166, 35.692026778516166,
+        ]
+        self.robot.joint_damping = [
+            0.25132741228, 0.25132741228,
+            0.25132741228, 0.25132741228, 0.25132741228, 0.25132741228,
+            0.25132741228, 0.25132741228, 0.25132741228, 0.25132741228,
+            3.60497756989125, 2.560161764834957, 2.1302109340993156,
+            4.806636759855, 4.260421868198631, 4.260421868198631,
+            3.60497756989125, 2.560161764834957, 2.1302109340993156,
+            4.806636759855, 4.260421868198631, 4.260421868198631,
+        ]
+        self.robot.effort_limit = [
+            6.0, 6.0,
+            14.0, 14.0, 14.0, 14.0,
+            14.0, 14.0, 14.0, 14.0,
+            68.0, 76.0, 38.3, 112.0, 38.3, 38.3,
+            68.0, 76.0, 38.3, 112.0, 38.3, 38.3,
+        ]
+
+
+register_task("k1_walk", K1WalkControllerCfg1())
+register_task("k1_walk_current_train", K1WalkCurrentTrainControllerCfg())
 register_task(
     "t1_walk", T1WalkControllerCfg1())
